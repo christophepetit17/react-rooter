80da69563d2ea81b2cc9ed73bd59938841aa92e2
christophepetit17 <cris17.mac@gmail.com>
2018-10-08 17:54:24 +0200

    react rooter quete

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+   
+    
+    render() { 
+        return ( <div>
+            <h1>Bienvenue sur ma page d'accueil</h1>
+        </div> );
+    }
+}
+ 
+export default Home;
